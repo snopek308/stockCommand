f68d13de3c8a612e03610c20063d508e445ea100
asnopek <asnopek@192.168.0.26>
2019-05-09 20:31:33 -0500

    Master Copy-5/9

---
+++ src/command/Order.java
@@ -0,0 +1,11 @@
+package command;
+
+/**
+ * This is the Command interface. Its implementations need to take an instance
+ * of the Receiver (the thing that gets commanded).
+ */
+public interface Order {
+
+    void execute();
+    void undo();
+}
